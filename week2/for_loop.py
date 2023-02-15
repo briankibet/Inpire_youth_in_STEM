2a79062e80c84c10755b3f77a33f94c829342016
brian <bklagatum@gmail.com>
2023-02-15 11:26:23 +0300

    loop for  python

--- week2/for_loop.py
+++ week2/for_loop.py
@@ -1,10 +1,10 @@
 sum = 0
 for i in range (0,10):
-print(i)
+  print(i)
 sum = sum + i
 print(sum)
 product = 1 
 for x in range (1,10):
-product = product * x 
+  product = product * x 
 print(product)
 #6! = 6*5*4*3*2*1
