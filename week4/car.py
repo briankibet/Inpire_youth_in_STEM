e630c7538946519372e6fcee55fd21ae61af4092
brian <bklagatum@gmail.com>
2023-02-28 08:32:39 +0300

    car,files,text.txt,odject_oriented_programming

---
+++ week4/car.py
@@ -0,0 +1,29 @@
+class car:
+    def__init__(self,model,make,year of manufacturer,engine_capacity):
+        self.model=model
+        self.model=year of manufacturer
+        self.model=engine_capacity
+        self.model=make
+def get_model(self):
+    return self.model
+def get_year(self):
+    return self.year of manufacturer
+def get_engine_capacity(self):
+    return self.get_engine_capacity
+def get_make(self):
+    return self.make
+
+
+
+
+
+
+
+car1 = car("demio","nissan",2018,1300)
+car1 = car("hillux","toyota",2020,5500)
+car1 = car("passat","vw",2009,260000)
+
+print(car1.get_model())
+print(car1.get_year())
+print(car1.get_model())
+print(car1.get_engine_capacity())
---
+++ week4/files.py
@@ -0,0 +1,17 @@
+''''
+f = open('C:\Users\Brian\Documents\Inspire-Youth-In-STEM\week4\text.txt')
+print(f.readline())
+filename  = r'C:\Users\Brian\Documents\Inspire-Youth-In-STEM\week4\text.txt'
+try:
+    with open(filename,'r+w',)
+    ''''
+
+
+
+
+
+
+
+file_name = "C:\\Users\\Brian\\Documents\\Inspire-Youth-In-STEM\week4\\text.txt"
+f = open(file_name,'r+w',encoding=None)
+print(file_name.readlines())
